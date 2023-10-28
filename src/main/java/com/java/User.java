edaa8c99fc39e2e03a545074aa77220f2c33c748
skwasim777 <wasimsk6061@gmail.com>
2023-10-28 16:48:13 +0530

    user class added to the spring application

---
+++ src/main/java/com/java/User.java
@@ -0,0 +1,18 @@
+package com.java;
+
+public class User {
+		private int id;
+		private String name,email;
+		public User(int id, String name, String email) {
+			super();
+			this.id = id;
+			this.name = name;
+			this.email = email;
+		}
+		@Override
+		public String toString() {
+			return "User [id=" + id + ", name=" + name + ", email=" + email + "]";
+		}
+		
+		
+}
